ffdefb0408a39cf5a80383bfd6ceed7972069bc5
Jonatas Tomaz <134023655+jhotmz@users.noreply.github.com>
2023-10-30 11:18:22 -0300

    Add files via upload

--- php/logar.php
+++ php/logar.php
@@ -1,31 +1,18 @@
 <?php
     include('conecta.php');
-    $stmt = $conn->prepare("SELECT * FROM tb_usuario WHERE nm_email = '".$_POST['email']."' and ds_senha = '".$_POST['senha']."'");
+    $stmt = $conn->prepare("SELECT * FROM tb_usuario WHERE nm_email = '".$_POST['email']."' and ds_senha = '".$_POST['senha']."'" );
     $stmt-> execute();
    
     if($stmt->rowCount()!=0){ 
     	session_start();
-         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
         $_SESSION['email'] = $_POST['email'];
         $_SESSION['nivel'] = $result['id_nivel'];
         $_SESSION['id_user'] = $result['id'];
-        
+    
         echo "<meta http-equiv='refresh' content='0'>";
     }
 
-
-    $stmt2 = $conn->prepare("SELECT * FROM tb_dentista WHERE nr_cpf = '".$_POST['email']."' and ds_senha = '".$_POST['senha']."'");
-    $stmt2-> execute();
-   
-    if($stmt2->rowCount()!=0){ 
-    	session_start();
-         $result = $stmt2->fetch(PDO::FETCH_ASSOC);
-        $_SESSION['email'] = $_POST['email'];
-        $_SESSION['nivel'] = $result['id_nivel'];
-        $_SESSION['id_user'] = $result['id_dentista'];
-        
-        echo "<meta http-equiv='refresh' content='0'>";
-    }
 
 
 ?>
--- php/validar-dentista.php
+++ php/validar-dentista.php
@@ -5,7 +5,7 @@ $id = $_POST['id'];
 $nivel = '3';
 
 try{
-$update = $conn->prepare("UPDATE tb_dentista SET id_nivel = :nivel WHERE id_dentista = :id");
+$update = $conn->prepare("UPDATE tb_usuario SET id_nivel = :nivel WHERE id = :id");
 $update->bindValue(':id', $id);
 $update->bindValue(':nivel', $nivel);
 $update->execute();
