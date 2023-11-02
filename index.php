19a123b1842389f2533da7f9dd10e70f62d9e5cd
botzin <fabiojosedelima011@gmail.com>
2023-11-02 08:25:52 -0300

    volto

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ session_start();
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Homeee</title>
+    <title>Home</title>
     <link rel="icon" type="image/x-icon" href="img/logo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
