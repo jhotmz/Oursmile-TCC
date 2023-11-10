2f71728b6b4e9dd487e136612aa07111240411dd
Jonatas Tomaz <jonatastomazc4@gmail.com>
2023-11-09 22:58:06 -0300

    Pesquisa de usuario

--- pag/listar_usuarioAdm.php
+++ pag/listar_usuarioAdm.php
@@ -86,20 +86,26 @@ include("nav.php");
           $stmt->execute();
           ?>
 
+
+
+
           <div class="box-container">
-            <div class="pesquisar">
-              <input type="search" placeholder="Pesquisar...">
-            </div>
             <div class="container">
               <div class="row mt-4">
                 <div class="col-lg-12">
                   <div>
-                    <h4>Listar Usuários</h4>
+                  <h1>Lista de Registros</h1>
                     <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalAdicionar">+ Adicionar usuário </button>
                   </div>
                 </div>
               </div>
               <hr>
+
+             
+    <input type="text" id="search" placeholder="Pesquisar">
+    <div id="records"></div>
+    <div class="pagination" id="pagination"></div>
+
               <div class="row">
                 <div class="col-lg-12">
 
@@ -167,7 +173,7 @@ include("nav.php");
                             <!-- FORMULÁRIO PARA DENTISTA -->
                           <div id="div3" class="hidden">
                             
-                            <form method="POST" id="formDentista" enctype="multipart/form-data" action="../php/add-dentAdm.php">
+                            <form method="POST" id="formDentista" enctype="multipart/form-data" action="../php/add-userAdm.php">
                               <input type="hidden" name="nivel_dentista" id="nivel_dentista" value="3">
 
                               <div class="form-group">
@@ -218,63 +224,7 @@ include("nav.php");
                     </div>
                   </div>
 
-                  <table class="table">
-                    <thead class="table-dark">
-                      <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Sobrenome</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Cargo</th>
-                        <th scope="col">Ações</th>
-                      </tr>
-                    </thead>
-                    <!-- LISTAR USUÁRIOS CADASTRADOS -->
 
-                    <tbody>
-                      <?php
-                      while ($listar_usuario = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                        extract($listar_usuario);
-
-                        if ($id_nivel === '1') {
-                          $id_nivel = "Usuário";
-                        } elseif ($id_nivel === '2') {
-                          $id_nivel = "Administrador";
-                        } elseif ($id_nivel === '3') {
-                          $id_nivel = "Dentista";
-                        } elseif ($id_nivel === '4'){
-                          $id_nivel = "Aguardando validação";
-                        }
-                        echo "<tr>";
-                        echo "<td>" . $id . "</td>";
-                        echo "<td>" . $nm_nome . "</td>";
-                        echo "<td>" . $nm_sobrenome . "</td>";
-                        echo "<td>" . $nm_email . "</td>";
-                        echo "<td>" . $id_nivel . "</td>";
-                      ?>
-                        <td>
-
-
-                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalEditar" data-id='<?php echo $id ?>' data-nivel='<?php echo $id_nivel ?>' data-nome='<?php echo $nm_nome ?>'>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H322.8c-3.1-8.8-3.7-18.4-1.4-27.8l15-60.1c2.8-11.3 8.6-21.5 16.8-29.7l40.3-40.3c-32.1-31-75.7-50.1-123.9-50.1H178.3zm435.5-68.3c-15.6-15.6-40.9-15.6-56.6 0l-29.4 29.4 71 71 29.4-29.4c15.6-15.6 15.6-40.9 0-56.6l-14.4-14.4zM375.9 417c-4.1 4.1-7 9.2-8.4 14.9l-15 60.1c-1.4 5.5 .2 11.2 4.2 15.2s9.7 5.6 15.2 4.2l60.1-15c5.6-1.4 10.8-4.3 14.9-8.4L576.1 358.7l-71-71L375.9 417z" />
-                            </svg>
-                          </button>
-
-                          <button type="button" class="btn btn-danger " data-bs-toggle="modal" data-bs-target="#modalApagar" data-id='<?php echo $id ?>' data-nome='<?php echo $nm_nome ?>'>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                              <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-                            </svg>
-                          </button>
-
-                        </td>
-                        </tr>
-
-
-                        <!-- FECHAR WHILE DA LISTAGEM -->
-                      <?php
-                      }
-                      ?>
 
                       <!-- Modal Editar Cargo -->
                       <div class="modal fade" id="modalEditar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -344,6 +294,35 @@ include("nav.php");
     <script src="../js/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> 
+
+    <script>
+        function fetchRecords(page, search = '') {
+            $.ajax({
+                url: '../php/exibir-user.php',
+                method: 'POST',
+                data: { page: page, search: search },
+                success: function(response) {
+                    $('#records').html(response);
+                }
+            });
+        }
+
+        $(document).ready(function() {
+            fetchRecords(1);
+
+            $(document).on('click', '.pagination a', function(event) {
+                event.preventDefault();
+                var page = $(this).data('page');
+                var search = $('#search').val();
+                fetchRecords(page, search);
+            });
+
+            $('#search').keyup(function() {
+                var search = $(this).val();
+                fetchRecords(1, search);
+            });
+        });
+    </script>
   <!-- FECHAR SESSION -->
 <?php
 }
